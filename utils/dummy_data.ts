6c4634889e8f58a257b85181426a6b5ec52baa0e
OlexandrSi <oleksandr.si@alphax.digital>
2022-11-08 15:50:43 +0200

    dummy data change

--- utils/dummy_data.ts
+++ utils/dummy_data.ts
@@ -1,5 +1,5 @@
 export default [
   {name: 'value', type: 'text', value: ''},
-  {name: 'value2', type: 'number', value: ''},
+  {name: 'value2', type: 'number', value: '123'},
   {name: 'value3', type: 'text', value: 'dsq'},
 ];
